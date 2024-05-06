99903b6ecc60681a6bb21e0d2a50db71bfdaae11
maomaochong199 <maomaochong199@gmail.com>
2024-05-06 23:00:42 +0800

    添加项目文件。

---
+++ Symlink/Symlink.Designer.cs
@@ -0,0 +1,217 @@
+﻿using System.Windows.Forms;
+
+namespace Symlink
+{
+    partial class Symlink
+    {
+        /// <summary>
+        /// 必要的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private OpenFileDialog openFileDialog1;
+        private TextBox textBox_Link;
+        private Button button_BrowseLink;
+        private Label label_Type;
+        private ComboBox comboBox_Type;
+        private Label label_Link;
+        private Label label_Source;
+        private TextBox textBox_Target;
+        private Button button_BrowseTarget;
+        private TextBox textBox_CMDCommand;
+        private Label label_CMDCommand;
+        private Button button_CreateLink;
+        private SaveFileDialog saveFileDialog1;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox_Link = new System.Windows.Forms.TextBox();
+            this.button_BrowseLink = new System.Windows.Forms.Button();
+            this.label_Type = new System.Windows.Forms.Label();
+            this.comboBox_Type = new System.Windows.Forms.ComboBox();
+            this.label_Link = new System.Windows.Forms.Label();
+            this.label_Source = new System.Windows.Forms.Label();
+            this.textBox_Target = new System.Windows.Forms.TextBox();
+            this.button_BrowseTarget = new System.Windows.Forms.Button();
+            this.textBox_CMDCommand = new System.Windows.Forms.TextBox();
+            this.label_CMDCommand = new System.Windows.Forms.Label();
+            this.button_CreateLink = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // folderBrowserDialog1
+            //// 
+            //this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            //// 
+            //// openFileDialog1
+            //// 
+            //this.openFileDialog1.FileName = "openFileDialog1";
+            //this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // textBox_Link
+            // 
+            this.textBox_Link.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_Link.Location = new System.Drawing.Point(125, 93);
+            this.textBox_Link.Multiline = true;
+            this.textBox_Link.Name = "textBox_Link";
+            this.textBox_Link.Size = new System.Drawing.Size(444, 56);
+            this.textBox_Link.TabIndex = 0;
+            // 
+            // button_BrowseLink
+            // 
+            this.button_BrowseLink.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_BrowseLink.Location = new System.Drawing.Point(575, 110);
+            this.button_BrowseLink.Name = "button_BrowseLink";
+            this.button_BrowseLink.Size = new System.Drawing.Size(75, 21);
+            this.button_BrowseLink.TabIndex = 1;
+            this.button_BrowseLink.Text = "浏览";
+            this.button_BrowseLink.UseVisualStyleBackColor = true;
+            this.button_BrowseLink.Click += new System.EventHandler(this.button_BrowseLink_Click);
+            // 
+            // label_Type
+            // 
+            this.label_Type.AutoSize = true;
+            this.label_Type.Location = new System.Drawing.Point(24, 9);
+            this.label_Type.Name = "label_Type";
+            this.label_Type.Size = new System.Drawing.Size(82, 15);
+            this.label_Type.TabIndex = 2;
+            this.label_Type.Text = "链接类型：";
+            // 
+            // comboBox_Type
+            // 
+            this.comboBox_Type.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox_Type.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox_Type.FormattingEnabled = true;
+            this.comboBox_Type.Location = new System.Drawing.Point(125, 6);
+            this.comboBox_Type.Name = "comboBox_Type";
+            this.comboBox_Type.Size = new System.Drawing.Size(444, 20);
+            this.comboBox_Type.TabIndex = 3;
+            this.comboBox_Type.SelectedIndexChanged += new System.EventHandler(this.comboBox_Type_SelectedIndexChanged);
+            // 
+            // label_Link
+            // 
+            this.label_Link.AutoSize = true;
+            this.label_Link.Location = new System.Drawing.Point(12, 113);
+            this.label_Link.Name = "label_Link";
+            this.label_Link.Size = new System.Drawing.Size(120, 15);
+            this.label_Link.TabIndex = 4;
+            this.label_Link.Text = "链接/目标位置：";
+            // 
+            // label_Source
+            // 
+            this.label_Source.AutoSize = true;
+            this.label_Source.Location = new System.Drawing.Point(18, 50);
+            this.label_Source.Name = "label_Source";
+            this.label_Source.Size = new System.Drawing.Size(105, 15);
+            this.label_Source.TabIndex = 5;
+            this.label_Source.Text = "目标/源位置：";
+            // 
+            // textBox_Target
+            // 
+            this.textBox_Target.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_Target.Location = new System.Drawing.Point(125, 31);
+            this.textBox_Target.Multiline = true;
+            this.textBox_Target.Name = "textBox_Target";
+            this.textBox_Target.Size = new System.Drawing.Size(444, 56);
+            this.textBox_Target.TabIndex = 6;
+            // 
+            // button_BrowseTarget
+            // 
+            this.button_BrowseTarget.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_BrowseTarget.Location = new System.Drawing.Point(573, 48);
+            this.button_BrowseTarget.Name = "button_BrowseTarget";
+            this.button_BrowseTarget.Size = new System.Drawing.Size(75, 21);
+            this.button_BrowseTarget.TabIndex = 7;
+            this.button_BrowseTarget.Text = "浏览";
+            this.button_BrowseTarget.UseVisualStyleBackColor = true;
+            this.button_BrowseTarget.Click += new System.EventHandler(this.button_BrowseTarget_Click);
+            // 
+            // textBox_CMDCommand
+            // 
+            this.textBox_CMDCommand.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_CMDCommand.Location = new System.Drawing.Point(125, 154);
+            this.textBox_CMDCommand.Multiline = true;
+            this.textBox_CMDCommand.Name = "textBox_CMDCommand";
+            this.textBox_CMDCommand.Size = new System.Drawing.Size(444, 95);
+            this.textBox_CMDCommand.TabIndex = 8;
+            this.textBox_CMDCommand.Click += new System.EventHandler(this.textBox_CMDCommand_Click);
+            // 
+            // label_CMDCommand
+            // 
+            this.label_CMDCommand.AutoSize = true;
+            this.label_CMDCommand.Location = new System.Drawing.Point(24, 189);
+            this.label_CMDCommand.Name = "label_CMDCommand";
+            this.label_CMDCommand.Size = new System.Drawing.Size(84, 15);
+            this.label_CMDCommand.TabIndex = 9;
+            this.label_CMDCommand.Text = "CMD 命令：";
+            // 
+            // button_CreateLink
+            // 
+            this.button_CreateLink.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_CreateLink.Location = new System.Drawing.Point(573, 189);
+            this.button_CreateLink.Name = "button_CreateLink";
+            this.button_CreateLink.Size = new System.Drawing.Size(75, 21);
+            this.button_CreateLink.TabIndex = 10;
+            this.button_CreateLink.Text = "创建链接";
+            this.button_CreateLink.UseVisualStyleBackColor = true;
+            this.button_CreateLink.Click += new System.EventHandler(this.button_CreateLink_Click);
+            // 
+            // saveFileDialog1
+            // 
+            //this.saveFileDialog1.OverwritePrompt = false;
+            //this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // Symlink
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(656, 261);
+            this.Controls.Add(this.button_CreateLink);
+            this.Controls.Add(this.label_CMDCommand);
+            this.Controls.Add(this.textBox_CMDCommand);
+            this.Controls.Add(this.button_BrowseTarget);
+            this.Controls.Add(this.textBox_Target);
+            this.Controls.Add(this.label_Source);
+            this.Controls.Add(this.label_Link);
+            this.Controls.Add(this.comboBox_Type);
+            this.Controls.Add(this.label_Type);
+            this.Controls.Add(this.button_BrowseLink);
+            this.Controls.Add(this.textBox_Link);
+            this.MinimumSize = new System.Drawing.Size(400, 300);
+            this.Name = "Symlink";
+            this.Text = "软链接创建器";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+#endregion
---
+++ Symlink/Symlink.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Diagnostics; // 用于执行CMD命令
+using System.IO; // 用于检查文件是否存在
+using System.Windows.Forms;
+
+// 作者: Octanum
+namespace Symlink
+{
+    public partial class Symlink : Form
+    {
+        public Symlink()
+        {
+            InitializeComponent();
+
+            // 设置窗体和textBox_Link允许拖放
+            this.AllowDrop = true;
+            this.textBox_Link.AllowDrop = true;
+            this.textBox_Target.AllowDrop = true;
+
+            // 为textBox_Link和textBox_Target添加事件处理程序
+            this.textBox_Link.DragEnter += new DragEventHandler(textBox_DragEnter);
+            this.textBox_Link.DragDrop += new DragEventHandler(textBox_DragDrop);
+            this.textBox_Target.DragEnter += new DragEventHandler(textBox_DragEnter);
+            this.textBox_Target.DragDrop += new DragEventHandler(textBox_DragDrop);
+
+        }
+
+        private void textBox_DragEnter(object sender, DragEventArgs e)
+        {
+            // 检查拖动数据是否包含文件
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy; // 允许复制操作
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None; // 不允许其他类型的拖动操作
+            }
+        }
+
+        private void textBox_DragDrop(object sender, DragEventArgs e)
+        {
+            // 获取拖动的文件数组
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            // 将第一个文件（或所有文件）的路径添加到文本框中
+            if (files != null && files.Length > 0)
+            {
+                // 由于sender是object类型，我们需要将其转换为TextBox
+                TextBox textBox = (TextBox)sender;
+                textBox.Text = files[0]; // 只添加第一个文件路径
+
+                // 如果需要添加所有文件的路径，可以使用以下代码：
+                // string filePaths = string.Join(Environment.NewLine, files);
+                // textBox.Text = filePaths;
+            }
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 设置所有选项
+            comboBox_Type.Items.Add("文件软链接（默认）");
+            comboBox_Type.Items.Add("目录软链接（/D）");
+            comboBox_Type.Items.Add("文件硬链接（/H）");
+            comboBox_Type.Items.Add("目录硬链接（/J）");
+
+            // 强制用户选择索引0，防止不选择任何选项
+            comboBox_Type.SelectedIndex = 0;
+
+            UpdateCMDCommand();
+        }
+
+        private void comboBox_Type_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // 为了避免用户混淆，切换选项后重置他们的选择
+            // 这样可以避免目录和文件的混合
+            textBox_Link.Text = "";
+            textBox_Target.Text = "";
+
+            UpdateCMDCommand();
+        }
+
+        private void button_BrowseLink_Click(object sender, EventArgs e)
+        {
+            if (comboBox_Type.SelectedIndex == 0 || comboBox_Type.SelectedIndex == 2) // 文件浏览/保存
+            {
+                DialogResult result = saveFileDialog1.ShowDialog();
+
+                if (result == DialogResult.OK)
+                {
+                    textBox_Link.Text = saveFileDialog1.FileName;
+
+                    UpdateCMDCommand();
+                }
+            }
+            else // 文件夹浏览
+            {
+                DialogResult result = folderBrowserDialog1.ShowDialog();
+
+                if (result == DialogResult.OK)
+                {
+                    textBox_Link.Text = folderBrowserDialog1.SelectedPath;
+
+                    UpdateCMDCommand();
+                }
+            }
+        }
+
+        private void button_BrowseTarget_Click(object sender, EventArgs e)
+        {
+            if (comboBox_Type.SelectedIndex == 0 || comboBox_Type.SelectedIndex == 2) // 文件浏览
+            {
+                DialogResult result = openFileDialog1.ShowDialog();
+
+                if (result == DialogResult.OK)
+                {
+                    textBox_Target.Text = openFileDialog1.FileName;
+
+                    UpdateCMDCommand();
+                }
+            }
+            else // 文件夹浏览
+            {
+                DialogResult result = folderBrowserDialog1.ShowDialog();
+
+                if (result == DialogResult.OK)
+                {
+                    textBox_Target.Text = folderBrowserDialog1.SelectedPath;
+
+                    UpdateCMDCommand();
+                }
+            }
+        }
+
+        private void button_CreateLink_Click(object sender, EventArgs e)
+        {
+            bool error = false;
+
+            if (textBox_Link.Text == "" || textBox_Target.Text == "") // 检查是否在未做任何操作的情况下点击了提交
+            {
+                error = true;
+                MessageBox.Show("必须指定文件/目录。");
+            }
+            else if (textBox_Link.Text == textBox_Target.Text)
+            {
+                error = true;
+                MessageBox.Show("链接不能与目标相同。");
+            }
+            else if (comboBox_Type.SelectedIndex == 0 || comboBox_Type.SelectedIndex == 2) // 文件链接
+            {
+                if (File.Exists(textBox_Link.Text)) // 这意味着用户可能想要用新的链接替换现有文件，但由于符号链接的安全性和限制，我们必须先删除它
+                {
+                    if (MessageBox.Show("替换 " + textBox_Link.Text + "?", "确认删除", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        File.Delete(textBox_Link.Text);
+                    }
+                    else
+                    {
+                        error = true;
+                        MessageBox.Show("不替换 " + textBox_Link.Text + " 无法继续。");
+                    }
+                }
+
+                if (!File.Exists(textBox_Target.Text)) // 检查目标文件是否存在
+                {
+                    error = true;
+                    MessageBox.Show("您在“目标”文本框中指定的文件不存在。");
+                }
+            }
+            else // 目录链接
+            {
+                if (Directory.Exists(textBox_Link.Text)) // 我们将替换用户可能创建的“新文件夹”
+                {
+                    if (MessageBox.Show("替换 " + textBox_Link.Text + "?", "确认删除", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        Directory.Delete(textBox_Link.Text);
+                    }
+                    else
+                    {
+                        error = true;
+                        MessageBox.Show("不替换 " + textBox_Link.Text + " 无法继续。");
+                    }
+                }
+
+                if (!Directory.Exists(textBox_Target.Text)) // 检查目标目录是否存在
+                {
+                    error = true;
+                    MessageBox.Show("您在“目标”文本框中指定的文件夹不存在。");
+                }
+            }
+
+
+            // 如果没有错误，我们现在创建实际链接
+            if (error == false)
+            {
+                // 创建一个新的进程对象
+                Process processSymlink = new Process();
+                // 创建进程启动信息对象
+                ProcessStartInfo startInfoSymlink = new ProcessStartInfo();
+                // 设置要运行的程序为cmd.exe
+                startInfoSymlink.FileName = "cmd.exe";
+
+                // 根据comboBox_Type的选择设置链接类型
+                string linkType = " ";
+                if (comboBox_Type.SelectedIndex == 0)
+                    linkType = " ";
+                else if (comboBox_Type.SelectedIndex == 1)
+                    linkType = " /D ";
+                else if (comboBox_Type.SelectedIndex == 2)
+                    linkType = " /H ";
+                else if (comboBox_Type.SelectedIndex == 3)
+                    linkType = " /J ";
+
+                // 构建命令行参数，/C表示执行完命令后关闭命令窗口
+                startInfoSymlink.Arguments = "/C MKLINK" + linkType + "\"" + textBox_Link.Text + "\" \"" + textBox_Target.Text + "\"";
+                // 设置不显示命令窗口
+                startInfoSymlink.CreateNoWindow = true;
+                // 设置不使用系统外壳程序执行命令
+                startInfoSymlink.UseShellExecute = false;
+                // 设置输出重定向，即使我们不输出内容，这样用户也不会看到它
+                startInfoSymlink.RedirectStandardOutput = true;
+
+                // 设置进程的启动信息
+                processSymlink.StartInfo = startInfoSymlink;
+                // 启动进程并执行命令
+                processSymlink.Start();
+            }
+        }
+
+        private void textBox_CMDCommand_Click(object sender, EventArgs e)
+        {
+            UpdateCMDCommand();
+        }
+
+        private void UpdateCMDCommand()
+        {
+            // 根据comboBox_Type的选择设置链接类型
+            string linkType = " ";
+            if (comboBox_Type.SelectedIndex == 0)
+                linkType = " ";
+            else if (comboBox_Type.SelectedIndex == 1)
+                linkType = " /D ";
+            else if (comboBox_Type.SelectedIndex == 2)
+                linkType = " /H ";
+            else if (comboBox_Type.SelectedIndex == 3)
+                linkType = " /J ";
+
+            // 更新textBox_CMDCommand文本框以显示MKLINK命令
+            textBox_CMDCommand.Text = "MKLINK" + linkType + "\"" + textBox_Link.Text + "\" \"" + textBox_Target.Text + "\"";
+        }
+
+        //private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        //{
+
+        //}
+
+        //private void openFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        //{
+
+        //}
+
+        //private void saveFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        //{
+
+        //}
+
+
+
+
+
+    }
+}
